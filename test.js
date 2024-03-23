33187e46a3fd335acdae6bdf48e91ba73ae26a5f
plugin-case <164586476+plugin-case@users.noreply.github.com>
2024-03-23 21:34:12 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+const hi = (title, version) =>
+      console.log(
+        `%c ${title} %c V${version} `,
+        'padding: 2px 1px; border-radius: 3px 0 0 3px; color: #fff; background: #606060; font-weight: bold;',
+        'padding: 2px 1px; border-radius: 0 3px 3px 0; color: #fff; background: #42c02e; font-weight: bold;',
+      );
+      
+hi('hello','0.0.6'); 
