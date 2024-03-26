c3ec3161ef141c1afbdb68ce8aa1b245a063750c
rlinhe <rlinhe@qq.com>
2024-03-26 23:02:20 +0800

    chor: set files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,15 @@
 export default {
   input: 'src/index.js',
-  output: {
-      file: './dist/vxetable-plugin.js',
+  output: [
+    {
+        file: './dist/vxetable-plugin.js',
+        format: 'iife',
+        name: 'vxeTablePlugin'
+    },
+    {
+      file: './dist/vxetable-plugin.min.js',
       format: 'iife',
       name: 'vxeTablePlugin'
-  }
+    },
+  ]
 };
